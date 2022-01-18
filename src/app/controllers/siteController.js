c2fa89b48ef40f1749d421124661cd1fbb13a14a
Đức Tâm <97541568+DevMyLove@users.noreply.github.com>
2022-01-19 01:30:23 +0700

    Update siteController.js

--- src/app/controllers/siteController.js
+++ src/app/controllers/siteController.js
@@ -12,7 +12,7 @@ class SiteController{
         res.render('page404');
     }
     search(req,res,next){
-        res.send('hello, my name Nguyen Duc Tam');
+        res.send('this is page search');
     }
 }
 module.exports = new SiteController;
